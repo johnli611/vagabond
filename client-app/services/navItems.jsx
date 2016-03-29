6112724bd850d56e44d7df4703d28dad3da0c915
John Li <j@peer.com>
2016-03-29 16:09:51 -0700

    add client-app directory

---
+++ client-app/services/navItems.jsx
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: 'About Me',
+    path: '/about'
+  },
+  {
+    name: 'Favorites',
+    path: '/favorites'
+  },
+  {
+    name: 'Bucket List',
+    path: '/bucket-list'
+  }
+];
